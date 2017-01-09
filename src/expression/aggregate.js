232fb630101d402c73b084e1694f23d0f1739f8f
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-09 14:45:48 +0100

    Add compile method

--- src/expression/aggregate.js
+++ src/expression/aggregate.js
@@ -1,4 +1,6 @@
 
+import { isString } from 'lodash' 
+
 /**
  * @class Aggregate
  */
@@ -7,16 +9,30 @@ export default class {
   /**
    * 
    * @param {String} method
-   * @param {String} column
-   * @param {String} name
+   * @param {String|Array} columns
+   * @param {String} as
    * @param {Boolean} isDistinct
    * @constructor
    */
-  constructor(method, column, name = null, isDistinct = false) {
-    this.name       = name
+  constructor(method, columns, as = null, isDistinct = false) {
+    if ( isString(columns) ) columns = columns.split(/\s*,\s*/)
+    
+    // TODO ensure aggregation method is valid
+    
+    this.name       = as
     this.method     = method
-    this.column     = column
+    this.columns     = columns
     this.isDistinct = isDistinct
+  }
+  
+  compile(compiler = null) {
+    // FIXME where to get the dialect
+    if (! compiler ) compiler = createCompiler(dialect)
+    
+    var columns = compiler.columnize(this.columns)
+    var distinct = this.isDistinct ? 'distinct ' : ''
+    
+    return compiler.alias(`${this.method}(${distinct}${columns})`, this.name)
   }
   
 }
