a9aca3c4ee7286fc7c408ab53a6ca06fb846769c
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-10 14:31:13 +0100

    Create expression/criterion/base.js

---
+++ src/expression/criterion/base.js
@@ -0,0 +1,16 @@
+
+import Expression from '../base'
+
+/**
+ * @class CriterionExpression
+ */
+export default class Criterion extends Expression {
+  
+  /**
+   * @constructor
+   */
+  constructor(column, operator, value, prefix = 'and', negate = false) {
+    
+  }
+  
+}
