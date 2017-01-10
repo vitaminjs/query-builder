e8a7335a56dceea8ea61e5f22ae42ef83355599f
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-10 16:28:00 +0100

    Update base.js

--- src/expression/criterion/base.js
+++ src/expression/criterion/base.js
@@ -1,5 +1,7 @@
 
+import { isString } from 'lodash'
 import Expression from '../base'
+import Column from '../column'
 
 /**
  * @class CriterionExpression
@@ -7,10 +9,42 @@ import Expression from '../base'
 export default class Criterion extends Expression {
   
   /**
+   * 
+   * @param {String|Expression} column
+   * @param {String} operator
+   * @param {Any} value
+   * @param {String} prefix
+   * @param {Boolean} negate
    * @constructor
    */
   constructor(column, operator, value, prefix = 'and', negate = false) {
+    super()
     
+    if ( isString(column) ) column = new Column(column)
+    
+    if (! (column instanceof Expression ) )
+      throw new TypeError("Invalid column expression")
+    
+    this.prefix = prefix
+    this.negate = negate
+    this.column = column
+    this.value = value
+    this.op = operator
+  }
+  
+  /**
+   * 
+   * @param {Compiler} compiler
+   * @return string
+   */
+  compile(compiler) {
+    var bool = this.prefix + ' '
+    var not = this.negate ? 'not ' : ''
+    var operator = compiler.operator(this.op)
+    var column = this.column.compile(compiler)
+    var value = compiler.parametrize(this.value)
+    
+    return bool + not + column + ` ${operator} ` + value
   }
   
 }
