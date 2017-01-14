4f85087018dc37a72bbc6bec293f7a6b4faf9c42
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-14 17:57:10 +0000

    Update expression helpers

---
+++ src/helpers/expression.js
@@ -0,0 +1,49 @@
+
+import { Raw as Expression, SubQuery } from '../expression'
+import { isString, isFunction } from 'lodash'
+import Builder from '../query/builder'
+import { Select } from '../query'
+
+/**
+ * A handler function for template strings
+ * 
+ * @param {Array} parts
+ * @param {Array} args
+ * @return {Raw}
+ */
+export function RAW(parts = [], ...args) {
+  if ( isString(parts) ) parts = parts.splice('?')
+
+  // TODO check for undefined values
+  
+  return new Expression(parts.join('?'), args)
+}
+
+/**
+ * 
+ * @param {Any} query
+ * @return {SubQuery} expression
+ * @throws {TypeError}
+ */
+export function SQ(query) {
+  // accept a function as a parameter
+  if ( isFunction(query) ) {
+    let fn = query
+    
+    fn(query = new Builder())
+  }
+  
+  // accept a builder instance
+  if ( query instanceof Builder )
+    query = query.build()
+  
+  // accept a Select query instance
+  if ( query instanceof Select )
+    query = new SubQuery(query)
+  
+  // and finally an instance of SubQuery expression
+  if ( query instanceof SubQuery ) return query
+  
+  // throws an error for invalid argument
+  throw new TypeError("Invalid sub query expression")
+}
