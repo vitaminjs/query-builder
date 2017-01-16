30f7b24293f7a21ce5f1707d70b642d9472f0574
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-16 17:16:16 +0000

    Add more conditional helpers

--- src/helpers/condition.js
+++ src/helpers/condition.js
@@ -1,62 +1,151 @@
 
-import { Basic } from '../criterion'
+import { Basic, IsNull, IsIn, Between, Exists, Like } from '../criterion'
+import { RAW, SQ } from './expression'
+import { isArray } from 'lodash'
 
 /**
  * 
- * @param {String|Expression} first
- * @param {Any} second
+ * @param {String|Expression} expr
+ * @param {Any} value
  * @return {Criterion}
  */
-export function EQ(first, second) {
-  return new Basic(first, '=', second)
+export function EQ(expr, value) {
+  return new Basic(expr, '=', value)
 }
 
 /**
  * 
- * @param {String|Expression} first
- * @param {Any} second
+ * @param {String|Expression} expr
+ * @param {Any} value
  * @return {Criterion}
  */
-export function NE(first, second) {
-  return new Basic(first, '!=', second)
+export function NE(expr, value) {
+  return new Basic(expr, '!=', value)
 }
 
 /**
  * 
- * @param {String|Expression} first
- * @param {Any} second
+ * @param {String|Expression} expr
+ * @param {Any} value
  * @return {Criterion}
  */
-export function GT(first, second) {
-  return new Basic(first, '>', second)
+export function GT(expr, value) {
+  return new Basic(expr, '>', value)
 }
 
 /**
  * 
- * @param {String|Expression} first
- * @param {Any} second
+ * @param {String|Expression} expr
+ * @param {Any} value
  * @return {Criterion}
  */
-export function LT(first, second) {
-  return new Basic(first, '<', second)
+export function LT(expr, value) {
+  return new Basic(expr, '<', value)
 }
 
 /**
  * 
- * @param {String|Expression} first
- * @param {Any} second
+ * @param {String|Expression} expr
+ * @param {Any} value
  * @return {Criterion}
  */
-export function GTE(first, second) {
-  return new Basic(first, '>=', second)
+export function GTE(expr, value) {
+  return new Basic(expr, '>=', value)
 }
 
 /**
  * 
- * @param {String|Expression} first
- * @param {Any} second
+ * @param {String|Expression} expr
+ * @param {Any} value
  * @return {Criterion}
  */
-export function LTE(first, second) {
-  return new Basic(first, '<=', second)
+export function LTE(expr, value) {
+  return new Basic(expr, '<=', value)
+}
+
+/**
+ * 
+ * @param {String|Expression} expr
+ * @returns {Criterion}
+ */
+export function ISNULL(expr) {
+  return new IsNull(expr)
+}
+
+/**
+ * 
+ * @returns {Criterion}
+ */
+export function ISTRUE() {
+  return RAW`1 = 1`
+}
+
+/**
+ * 
+ * @returns {Criterion}
+ */
+export function ISFALSE() {
+  return RAW`1 = 0`
+}
+
+/**
+ * 
+ * @param {String|Expression} expr
+ * @param {Any} values
+ * @returns {Criterion}
+ */
+export function IN(expr, values) {
+  if (! isArray(value) ) values = SQ(values)
+
+  return new IsIn(expr, values)
+}
+
+/**
+ * 
+ * @param {String|Expression} expr
+ * @param {Any} lower
+ * @param {Any} higher
+ * @returns {Criterion}
+ */
+export function BETWEEN(expr, lower, higher) {
+  return new Between(expr, [lower, higher])
+}
+
+/**
+ * 
+ * @param {Any} query
+ * @returns {Criterion}
+ */
+export function EXISTS(query) {
+  return new Exists(SQ(query))
+}
+
+/**
+ * 
+ * @param {String|Expression} expr
+ * @param {String} patern
+ * @returns {Criterion}
+ */
+export function LIKE(expr, patern) {
+  return new LIKE(expr, patern)
+}
+
+/**
+ * 
+ * @param {String|Expression} expr
+ * @param {String} value
+ * @returns {Criterion}
+ */
+export function STARTSWITH(expr, value) {
+  return LIKE(expr, value.replace(/(_|%)/g, '\\$1') + '%')
+}
+
+/**
+ * 
+ * @param {String|Expression} expr
+ * @param {String} value
+ * @returns {Criterion}
+ */
+export function ENDSWITH(expr, value) {
+  return LIKE(expr, '%' + value.replace(/(_|%)/g, '\\$1'))
 }
