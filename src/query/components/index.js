e795676bf01521cb068fa634c970f26045df177c
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-16 11:42:16 +0000

    Add orders container

--- src/query/components/index.js
+++ src/query/components/index.js
@@ -1,6 +1,7 @@
 
 import Joins from './joins'
 import Unions from './unions'
+import Orders from './orders'
 import Tables from './tables'
 import Columns from './columns'
 import Container from './container'
@@ -9,4 +10,4 @@ import Container from './container'
 export default Container
 
 // export components
-export { Joins, Columns, Tables, Unions }
+export { Joins, Columns, Tables, Unions, Orders }
