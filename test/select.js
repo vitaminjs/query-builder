7873c5d105c0434dd20adb546b01e7b1a4e06558
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-27 14:01:01 +0000

    Update tests

--- test/select.js
+++ test/select.js
@@ -4,43 +4,62 @@ var Builder = require('../lib/index')
 var fn = require('../lib/helpers')
 var support = require('./support')
 var qb = () => new Builder()
-var RAW = fn.RAW
 var COUNT = fn.COUNT
+var RAW   = fn.RAW
+var MAX   = fn.MAX
+var SQ    = fn.SQ
+var C     = fn.C
 
 describe("test building select queries:", () => {
   
   describe("test select():", () => {
     
     support.test("returns empty string for empty queries", qb().select(), {
-      mysql: '',
-      mssql: '',
+      pg:     '',
+      mysql:  '',
+      mssql:  '',
       sqlite: '',
       oracle: '',
-      postgre: '',
     })
     
     support.test("accepts literal expressions", qb().select(1, false, 'xXx'), {
-      mysql: 'select 1, 0, xXx',
-      mssql: 'select 1, 0, xXx',
+      pg:     'select 1, 0, xXx',
+      mysql:  'select 1, 0, xXx',
+      mssql:  'select 1, 0, xXx',
       sqlite: 'select 1, 0, xXx',
-      oracle: 'select 1, 0, xXx',
-      postgre: 'select 1, 0, xXx',
+      oracle: 'select 1, 0, xXx from dual',
     })
     
     support.test("accepts raw expressions", qb().select(RAW('1 + ? as "operation"', 2)), {
-      mysql: 'select 1 + ? as "operation"',
-      mssql: 'select 1 + @1 as "operation"',
+      mysql:  'select 1 + ? as "operation"',
+      mssql:  'select 1 + @1 as "operation"',
       sqlite: 'select 1 + $1 as "operation"',
       oracle: 'select 1 + :1 as "operation"',
-      postgre: 'select 1 + $1 as "operation"',
+      pg:     'select 1 + $1 as "operation"',
     }, [2])
     
-    support.test("accepts aggregates", qb().select(COUNT('foo').as('foo_count')), {
-      mysql: 'select count(foo) as `foo_count`',
-      mssql: 'select count(foo) as [foo_count]',
-      sqlite: 'select count(foo) as "foo_count"',
-      oracle: 'select count(foo) "foo_count"',
-      postgre: 'select count(foo) as "foo_count"',
+    support.test("accepts aggregates", qb().select(COUNT('foo').distinct().as('foo_count')), {
+      oracle: 'select count(distinct foo) "foo_count"',
+      mysql:  'select count(distinct foo) as `foo_count`',
+      mssql:  'select count(distinct foo) as [foo_count]',
+      sqlite: 'select count(distinct foo) as "foo_count"',
+      pg:     'select count(distinct foo) as "foo_count"',
+    })
+    
+    support.test("accepts sub queries", qb().select(SQ('select count(*) from table').as('column')), {
+      pg:     'select (select count(*) from table) as "column"',
+      mysql:  'select (select count(*) from table) as `column`',
+      mssql:  'select (select count(*) from table) as [column]',
+      sqlite: 'select (select count(*) from table) as "column"',
+      oracle: 'select (select count(*) from table) "column"',
+    })
+
+    support.test("accepts column expressions", qb().select(C('foo').as('bar'), MAX(C('baz')).as('dummy')), {
+      pg:     'select "foo" as "bar", max("baz") as "dummy"',
+      mysql:  'select `foo` as `bar`, max(`baz`) as `dummy`',
+      mssql:  'select [foo] as [bar], max([baz]) as [dummy]',
+      sqlite: 'select "foo" as "bar", max("baz") as "dummy"',
+      oracle: 'select "foo" "bar", max("baz") "dummy"',
     })
     
   })
