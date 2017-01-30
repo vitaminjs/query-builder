85d10fbe9f11005b8ffdeb86e10a42e1cadadde1
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-30 11:17:29 +0000

    Update query/index.js

--- src/query/index.js
+++ src/query/index.js
@@ -10,24 +10,3 @@ export default Query
 
 // export query classes
 export { Select, Insert, Update, Delete }
-
-/**
- * Query factory by type
- * 
- * @param {String} type
- * @returns {Query}
- * @throws {TypeError}
- */
-export function createQuery(type) {
-  
-  if ( type === 'select' ) return new Select()
-  
-  if ( type === 'insert' ) return new Insert()
-  
-  if ( type === 'update' ) return new Update()
-  
-  if ( type === 'delete' ) return new Delete()
-  
-  throw new TypeError("Invalid query type")
-  
-}
