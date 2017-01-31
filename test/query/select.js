c63fd45943194e8e04b4265fd7b48d30c16651ce
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-01-31 19:33:48 +0000

    Update select tests

--- test/query/select.js
+++ test/query/select.js
@@ -7,19 +7,24 @@ var COUNT = fn.COUNT
 var RAW   = fn.RAW
 var MAX   = fn.MAX
 var SQ    = fn.SQ
+var T     = fn.T
 var C     = fn.C
 
 describe("test building select queries:", () => {
   
   describe("test select():", () => {
     
-    support.test("returns empty string for empty queries", qb.select(), {
-      pg:     '',
-      mysql:  '',
-      mssql:  '',
-      sqlite: '',
-      oracle: '',
-    })
+    support.test(
+      "returns empty string for empty queries",
+      qb.select(),
+      {
+        pg:     '',
+        mysql:  '',
+        mssql:  '',
+        sqlite: '',
+        oracle: '',
+      }
+    )
     
     support.test("accepts literal expressions", qb.select(1, false, 'xXx'), {
       pg:     'select 1, 0, xXx',
@@ -53,14 +58,132 @@ describe("test building select queries:", () => {
       oracle: 'select (select count(*) from table) "column" from dual',
     })
 
-    support.test("accepts column expressions", qb.select(C('foo').as('bar'), MAX(C('baz')).as('dummy')), {
+    support.test("accepts column expressions", qb.select(C('foo as bar'), MAX(C('baz')).as('dummy')), {
       pg:     'select "foo" as "bar", max("baz") as "dummy"',
       mysql:  'select `foo` as `bar`, max(`baz`) as `dummy` from dual',
       mssql:  'select [foo] as [bar], max([baz]) as [dummy]',
       sqlite: 'select "foo" as "bar", max("baz") as "dummy"',
       oracle: 'select "foo" "bar", max("baz") "dummy" from dual',
     })
+
+    support.test("selects distinct columns", qb.select('foo', C('bar')).distinct(), {
+      pg:     'select distinct foo, "bar"',
+      mysql:  'select distinct foo, `bar` from dual',
+      mssql:  'select distinct foo, [bar]',
+      sqlite: 'select distinct foo, ""bar"',
+      oracle: 'select distinct foo, "bar" from dual',
+    })
     
   })
+
+  describe("test from():", () => {
+
+    support.test("accepts a raw table expression", qb.select().from('table'), {
+      pg:     'select * from table',
+      mysql:  'select * from table',
+      mssql:  'select * from table',
+      sqlite: 'select * from table',
+      oracle: 'select * from table',
+    })
+
+    support.test("adds a table expression", qb.selectFrom(T('schema.table as alias')), {
+      pg:     'select * from "schema"."table" as "alias"',
+      mysql:  'select * from `schema`.`table` as `alias`',
+      mssql:  'select * from [schema].[table] as [alias]',
+      sqlite: 'select * from "schema"."table" as "alias"',
+      oracle: 'select * from "schema"."table" "alias"',
+    })
+
+    support.test("accepts more than one table for `from` clause", qb.selectFrom('foo', 'bar'), {
+      pg:     'select * from foo, bar',
+      mysql:  'select * from foo, bar',
+      mssql:  'select * from foo, bar',
+      sqlite: 'select * from foo, bar',
+      oracle: 'select * from foo, bar',
+    })
+
+    support.test("accepts raw expressions", qb.select().from(RAW('schema.table t')), {
+      pg:     'select * from schema.table t',
+      mysql:  'select * from schema.table t',
+      mssql:  'select * from schema.table t',
+      sqlite: 'select * from schema.table t',
+      oracle: 'select * from schema.table t',
+    })
+
+    support.test("accepts sub queries", qb.selectFrom(q => q.select('*').from('a_table').as('t')), {
+      pg:     'select * from (select * from a_table) as "t"',
+      mysql:  'select * from (select * from a_table) as `t`',
+      mssql:  'select * from (select * from a_table) as [t]',
+      sqlite: 'select * from (select * from a_table) as "t"',
+      oracle: 'select * from (select * from a_table) "t"',
+    })
+
+  })
+
+  describe("test join():", () => {
+
+    support.test(
+      "adds a basic join clause",
+      qb.select().from('posts p').join('users a', 'p.author_id', 'a.id'),
+      {
+        pg:     'select * from posts p inner join users a on p.author_id = a.id',
+        mysql:  'select * from posts p inner join users a on p.author_id = a.id',
+        mssql:  'select * from posts p inner join users a on p.author_id = a.id',
+        sqlite: 'select * from posts p inner join users a on p.author_id = a.id',
+        oracle: 'select * from posts p inner join users a on p.author_id = a.id',
+      }
+    )
+
+    support.test(
+      "adds a raw join expression",
+      qb.from(T('table1')).join(RAW`natural full join ${T('table2')}`),
+      {
+        pg:     'select * from "table1" natural full join "table2"',
+        mysql:  'select * from `table1` natural full join `table2`',
+        mssql:  'select * from [table1] natural full join [table2]',
+        sqlite: 'select * from "table1" natural full join "table2"',
+        oracle: 'select * from "table1" natural full join "table2"',
+      }
+    )
+
+    support.test(
+      "adds a table join with more than one condition",
+      qb.selectFrom('people p').join('category c', cr => cr.on('p.salary', '>=', 'c.min_salary').on('p.salary', '<=', 'c.max_salary')),
+      {
+        pg:     'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        mysql:  'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        mssql:  'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        sqlite: 'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        oracle: 'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+      }
+    )
+
+    support.test(
+      "adds a table join with raw conditions",
+      qb.selectFrom('people p').join('category c', RAW('p.salary >= c.min_salary and p.salary <= c.max_salary').wrap()),
+      {
+        pg:     'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        mysql:  'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        mssql:  'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        sqlite: 'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+        oracle: 'select * from people p inner join category c on (p.salary >= c.min_salary and p.salary <= c.max_salary)',
+      }
+    )
+
+    support.test(
+      "adds a join with a sub query",
+      qb.selectFrom('table1').crossJoin(SQ(q => q.select().from('table2'))),
+      {
+        pg:     'select * from table1 cross join (select * from table2)',
+        mysql:  'select * from table1 cross join (select * from table2)',
+        mssql:  'select * from table1 cross join (select * from table2)',
+        sqlite: 'select * from table1 cross join (select * from table2)',
+        oracle: 'select * from table1 cross join (select * from table2)',
+      }
+    )
+
+  })
+
+  describe("test where():", () => {})
   
 })
