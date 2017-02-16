7a7737ac924ae1f54043f345f44bed4a8cb72164
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-02-16 20:04:58 +0000

    Update query/insert.js

--- src/query/base.js
+++ src/query/base.js
@@ -6,6 +6,38 @@ import { isString } from 'lodash'
  * @class BaseQuery
  */
 export default class Query {
+
+  /**
+   * 
+   * @constructor
+   */
+  constructor() {
+    this._options = {}
+  }
+
+  /**
+   * Set/Get a query option
+   * 
+   * @param {String} name
+   * @param {Any} value
+   * @returns {Any|Query}
+   */
+  option(name, value = undefined) {
+    if ( undefined !== value ) {
+      this._options[name] = value
+      return this
+    }
+
+    return this._options[name]
+  }
+
+  /**
+   * 
+   * @returns {Object}
+   */
+  getOptions() {
+    return this._options
+  }
   
   /**
    * 
--- src/query/insert.js
+++ src/query/insert.js
@@ -1,10 +1,12 @@
 
+import Expression, { Literal } from '../expression'
+import { isString, isEmpty } from 'lodash'
 import Query from './base'
 
 /**
  * @class InsertQuery
  */
-export default class extends Query {
+export default class Insert extends Query {
   
   /**
    * 
@@ -13,7 +15,117 @@ export default class extends Query {
   constructor() {
     super()
     
+    this._data = null
+    this._table = null
+    this._columns = []
+  }
+
+  /**
+   * 
+   * @param {Any} data
+   * @returns {Insert}
+   */
+  values(data) {
+    this._data = data
+    return this
+  }
+
+  /**
+   * 
+   * @returns {Any}
+   */
+  getValues() {
+    return this._data
+  }
+
+  /**
+   * 
+   * @returns {Boolean}
+   */
+  hasValues() {
+    return !isEmpty(this._data)
+  }
+
+  /**
+   * 
+   * @returns {Insert}
+   */
+  defaultValues() {
+    return this.option('default values', true)
+  }
+
+  /**
+   * 
+   * @param {String|Expression} table
+   * @param {String|Expression} columns
+   * @returns {Insert}
+   */
+  into(table, ...columns) {
+    if ( isString(table) )
+      table = new Literal(table)
     
+    // ensure the table expression
+    if (! (table instanceof Expression) )
+      throw new TypeError("Invalid insert expression")
+    
+    // map the column names
+    columns.forEach(value => isString(value) ? new Literal(value) : value)
+
+    this._columns = columns
+    this._table = table
+    return this
+  }
+
+  /**
+   * 
+   * @returns {String|Expression}
+   */
+  getTable() {
+    return this._table
+  }
+
+  /**
+   * 
+   * @returns {Boolean}
+   */
+  hasTable() {
+    return this._table != null
+  }
+
+  /**
+   * 
+   * @returns {Array}
+   */
+  getColumns() {
+    return this._columns
+  }
+
+  /**
+   * 
+   * @returns {Boolean}
+   */
+  hasColumns() {
+    return !isEmpty(this._columns)
+  }
+
+  /**
+   * 
+   * @param {String[]} output
+   * @returns {Insert}
+   */
+  returning(...output) {
+    return this.option('returning', output)
+  }
+
+  /**
+   * 
+   * @param {Compiler} compiler
+   * @returns {String}
+   */
+  compile(compiler) {
+    if (! this._table ) return ''
+
+    return compiler.compileInsertQuery(this)
   }
   
 }
