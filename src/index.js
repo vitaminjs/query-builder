79240fe876e32406722a795763286ca7f58469ab
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-02-18 18:23:22 +0000

    Add missing query builder methods

--- src/index.js
+++ src/index.js
@@ -1,22 +1,69 @@
 
-import { Select } from './query'
+import { Select, Insert, Delete, Update } from './query'
 
 const builder = {
 
   /**
    * 
+   * @param {Any} value
    * @returns {Select}
    */
-  select() {
-    return new Select().select(...arguments)
+  select(...value) {
+    return new Select().setColumns(value)
   },
 
   /**
    * 
+   * @param {Any} value
    * @return {Select}
    */
-  selectFrom() {
-    return this.select().from(...arguments)
+  selectFrom(...value) {
+    return this.select().setTables(value)
+  },
+
+  /**
+   * 
+   * @param {Object|Array} data
+   * @returns {Insert}
+   */
+  insert(data) {
+    return new Insert().values(data)
+  },
+
+  /**
+   * 
+   * @param {String|Expression} table
+   * @param {String|Expression} columns
+   * @returns {Insert}
+   */
+  insertInto(table, ...columns) {
+    return new Insert().into(table, ...columns)
+  },
+
+  /**
+   * 
+   * @param {String|Expression} table
+   * @returns {Update}
+   */
+  update(table) {
+    return new Update().setTable(table)
+  },
+
+  /**
+   * 
+   * @param {String|Expression} table
+   * @returns {Delete}
+   */
+  deleteFrom(table) {
+    return new Delete().setTable(table)
+  },
+
+  /**
+   * 
+   * @see deleteFrom()
+   */
+  delete(table) {
+    return this.deleteFrom(table)
   }
 
 }
