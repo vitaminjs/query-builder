c54e080b145c8877c68cd857facabcb10ea16cd5
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-02-18 17:33:44 +0000

    Update queries

--- src/query/delete.js
+++ src/query/delete.js
@@ -1,10 +1,12 @@
 
+import Expression, { Literal } from '../expression'
+import { isString } from 'lodash'
 import Query from './base'
 
 /**
  * @class DeleteQuery
  */
-export default class extends Query {
+export default class Delete extends Query {
   
   /**
    * 
@@ -13,7 +15,79 @@ export default class extends Query {
   constructor() {
     super()
     
+    this._conditions  = null
+    this._table       = null
+  }
+
+  /**
+   * 
+   * @param {String|Expression} value
+   * @returns {Delete}
+   */
+  from(value) {
+    return this.setTable(value)
+  }
+
+  /**
+   * 
+   * @returns {Expression}
+   */
+  getTable() {
+    return this._table
+  }
+
+  /**
+   * 
+   * @returns {Boolean}
+   */
+  hasTable() {
+    return this._table != null
+  }
+
+  /**
+   * 
+   * @param {String|Expression} value
+   * @returns {Update}
+   */
+  setTable(value) {
+    if ( isString(value) )
+      value = new Literal(value)
     
+    // ensure the table expression
+    if (! (value instanceof Expression) )
+      throw new TypeError("Invalid update expression")
+
+    this._table = value
+    return this
+  }
+
+  /**
+   * 
+   * @returns {Delete}
+   */
+  where() {
+    // TODO
+    return this
+  }
+
+  /**
+   * 
+   * @returns {Delete}
+   */
+  orWhere() {
+    // TODO
+    return this
+  }
+
+  /**
+   * 
+   * @param {Compiler} compiler
+   * @returns {String}
+   */
+  compile(compiler) {
+    if ( this._table == null ) return ''
+
+    return compiler.compileDeleteQuery(this)
   }
   
 }
--- src/query/update.js
+++ src/query/update.js
@@ -1,10 +1,12 @@
 
+import Expression, { Literal } from '../expression'
+import { isString } from 'lodash'
 import Query from './base'
 
 /**
  * @class UpdateQuery
  */
-export default class extends Query {
+export default class Update extends Query {
   
   /**
    * 
@@ -13,7 +15,91 @@ export default class extends Query {
   constructor() {
     super()
     
+    this._conditions  = null
+    this._table       = null
+    this._data        = {}
+  }
+
+  /**
+   * 
+   * @returns {Expression}
+   */
+  getTable() {
+    return this._table
+  }
+
+  /**
+   * 
+   * @returns {Boolean}
+   */
+  hasTable() {
+    return this._table != null
+  }
+
+  /**
+   * 
+   * @param {String|Expression} value
+   * @returns {Update}
+   */
+  setTable(value) {
+    if ( isString(value) )
+      value = new Literal(value)
     
+    // ensure the table expression
+    if (! (value instanceof Expression) )
+      throw new TypeError("Invalid update expression")
+
+    this._table = value
+    return this
+  }
+  
+  /**
+   * 
+   * @param {String|Object} key
+   * @param {Any} value
+   * @returns {Update}
+   */
+  set(key, value = undefined) {
+    // TODO
+    return this
+  }
+
+  /**
+   * 
+   * @returns {Delete}
+   */
+  where() {
+    // TODO
+    return this
+  }
+
+  /**
+   * 
+   * @returns {Delete}
+   */
+  orWhere() {
+    // TODO
+    return this
+  }
+
+  /**
+   * 
+   * @param {String} output
+   * @returns {Update}
+   */
+  returning(...output) {
+    return this.option('returning', output)
+  }
+
+  /**
+   * 
+   * @param {Compiler} compiler
+   * @returns {String}
+   */
+  compile(compiler) {
+    if ( this._table == null ) return ''
+
+    return compiler.compileUpdateQuery(this)
   }
   
 }
