69bfa6ca95260fd2aae7a47702091683680761c7
absolux <a.mouhassine@gmail.com>
2017-02-19 13:35:05 +0000

    Update test/query/insert.js

--- test/query/insert.js
+++ test/query/insert.js
@@ -39,5 +39,40 @@ describe("test building insert queries:", () => {
     },
     ['foo', 30, 'bar', 40]
   )
+  
+  support.test(
+    "inserts an new row with default values",
+    qb.insertInto('people').defaultValues(),
+    {
+      pg:     'insert into people default values',
+      mysql:  'insert into people () values ()',
+      mssql:  'insert into people default values',
+      sqlite: 'insert into people default values',
+    }
+  )
+
+  support.test(
+    "replaces missing bindings with default for multirow insert",
+    qb.insert([{ x: 20 }, { y: 40 }, { x: 10, y: 30 }]).into('coords'),
+    {
+      pg:     'insert into coords (x, y) values ($1, default), (default, $2), ($3, $4)',
+      mysql:  'insert into coords (x, y) values (?, default), (default, ?), (?, ?)',
+      mssql:  'insert into coords (x, y) values (@1, default), (default, @2), (@3, @4)',
+      sqlite: 'insert into coords (x, y) values ($1, null), (null, $2), ($3, $4)',
+    },
+    [20, 40, 10, 30]
+  )
+
+  support.test(
+    "adds a returning clause",
+    qb.insert({ fname: "foo", lname: 'bar' }).into('users').returning('id'),
+    {
+      pg:     'insert into users (fname, lname) values ($1, $2) returning id',
+      mysql:  'insert into users (fname, lname) values (?, ?)',
+      mssql:  'insert into users (fname, lname) output id values (@1, @2)',
+      sqlite: 'insert into users (fname, lname) values ($1, $2)',
+    },
+    ['foo', 'bar']
+  )
 
 })
