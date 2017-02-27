2e80a84abb5439da97ecee9919cbefe73f3b543f
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-02-27 13:15:52 +0000

    Use the question mark as placeholder for SQLite

--- src/compiler/sqlite.js
+++ src/compiler/sqlite.js
@@ -10,26 +10,6 @@ export default class extends Compiler {
   
   /**
    * 
-   * @param {Object} options
-   * @constructor
-   */
-  constructor(options = {}) {
-    super(options)
-
-    this.paramCount = 1
-  }
-  
-  /**
-   * Default parameter placeholder
-   * 
-   * @type {String}
-   */
-  get placeholder() {
-    return '$' + this.paramCount++
-  }
-
-  /**
-   * 
    * @param {Select} query
    * @returns {String}
    */
