153ae74abbb335f332b6acccf1364b2152fdb56f
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-07-07 18:29:32 +0100

    Add files via upload

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,33 +1,32 @@
 
-import Base from './base'
 import Mysql from './mysql'
 import Mssql from './mssql'
 import Sqlite from './sqlite'
 import Postgre from './postgre'
 
-// export the base compiler as default 
-export default Base
+// export the base compiler as default
+export { default as default } from './base'
 
 /**
  * Create a compiler for the given dialect
- * 
+ *
  * @param {String} dialect
  * @param {Object} options
  * @returns {Compiler}
  * @throws {TypeError}
  */
-export function createCompiler(dialect, options = {}) {
-  switch ( dialect.toLowerCase() ) {
+export function createCompiler (dialect, options = {}) {
+  switch (dialect.toLowerCase()) {
     case 'mysql': return new Mysql(options)
-    
+
     case 'mssql': return new Mssql(options)
-    
+
     case 'sqlite': return new Sqlite(options)
-    
+
     case 'pg':
-    case 'postgre': 
+    case 'postgre':
     case 'postgresql': return new Postgre(options)
   }
-  
-  throw new TypeError("Unknown compiler dialect")
+
+  throw new TypeError('Unknown compiler dialect')
 }
