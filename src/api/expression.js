28b80dfbafdd02eb485a3457dc89895b4ebcc4bd
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-07-07 18:31:18 +0100

    Add files via upload

--- src/api/expression.js
+++ src/api/expression.js
@@ -1,144 +1,56 @@
 
-import { isFunction, isArray, isString, isPlainObject, isUndefined, trim } from 'lodash'
-import Expression, { Literal, SubQuery, Column, Table, Escaped, Values } from '../expression'
-import Query from '../query'
+import { Identifier, Literal, Alias, Function } from '../expression'
 
 /**
- * A handler function for template strings
- * 
- * @param {String|Array} expr
- * @param {Array} args
+ * @param {String} expr
+ * @param {Array<Any>} values
  * @returns {Literal}
  */
-export function raw(expr, ...args) {
-  // handle template strings
-  if ( isArray(expr) )
-    expr = expr.join('?')
-  
-  // parse named parameters
-  if ( isPlainObject(args[0]) ) {
-    let obj = args[0]
-    args = []
-
-    expr = expr.replace(/[:@](\w+)/gi, (match, attr) => {
-      // we return the match string in case the attribute is undefined
-      if ( isUndefined(obj[attr]) ) return match
-      
-      // append the value of the attribute
-      args.push(obj[attr])
-
-      // return the basic placeholder
-      return '?'
-    })
-  }
-
-  return new Literal(expr, args)
+export function raw (expr, ...values) {
+  return new Literal(expr, values)
 }
 
 /**
- * 
- * @param {Any} value
- * @returns {Expression}
- * @throws {TypeError}
+ * @param {String} expr
+ * @returns {Identifier}
  */
-export function sq(value) {
-  return createSubQuery(value)
+export function id (expr) {
+  return new Identifier(expr)
 }
 
 /**
- * 
- * @param {Any} value
- * @returns {Expression}
- * @throws {TypeError}
- */
-export function cte(value) {
-  return createSubQuery(value).isCTE()
-}
-
-/**
- * 
- * @param {String} value
- * @returns {Column}
- */
-export function column(value) {
-  var alias = ''
-  
-  if ( value.indexOf(' as ') > 0 ) {
-    [value, alias] = value.split(' as ').map(trim)
-  }
-  
-  return new Column(value).as(alias)
-}
-
-/**
- * 
- * @param {String} value
- * @returns {Table}
- */
-export function table(value) {
-  var alias = ''
-
-  if ( value.indexOf(' as ') > 0 ) {
-    [value, alias] = value.split(' as ').map(trim)
-  }
-  
-  return new Table(value).as(alias)
-}
-
-/**
- * 
- * @param {Any} value
- * @returns {Escaped}
- */
-export function esc(value) {
-  return new Escaped(value)
-}
-
-/**
- * 
  * @param {Any} value
  * @param {String} type
- * @returns {Expression}
+ * @returns {Literal}
  */
-export function cast(value, type) {
+export function cast (value, type) {
   return raw(`cast(? as ${type})`, value)
 }
 
 /**
- * 
- * @param {Array} data
+ * @param {String} name
+ * @param {Array<Any>} args
  * @returns {Expression}
  */
-export function values (data) {
-  return new Values(data)
+export function func (name, ...args) {
+  return new Function(name, args)
+  // return raw(`${name}(${args.map(() => '?').join(', ')})`, args)
 }
 
 /**
- * 
  * @param {Any} value
- * @returns {Expression}
- * @throws {TypeError}
- * @private
+ * @param {String} name
+ * @param {Array<Any>} columns
+ * @returns {Alias}
  */
-function createSubQuery(value) {
-  // accept a raw query string
-  if ( isString(value) )
-    value = Literal.from(value)
-  
-  // accept a function as a parameter
-  if ( isFunction(value) ) {
-    let fn = value
-    
-    fn(value = new Select())
-  }
-  
-  // accept a Select query instance
-  if ( value instanceof Query || value instanceof Expression )
-    value = new SubQuery(value)
-  
-  // and finally an instance of SubQuery expression
-  if ( value instanceof SubQuery ) return value
-  
-  // throws an error for invalid argument
-  throw new TypeError("Invalid sub query expression")
+export function alias (value, name, ...columns) {
+  return new Alias(value, name, columns)
+}
+
+/**
+ * @param {Any} value
+ * @returns {Literal}
+ */
+export function param (value) {
+  return raw('?', value)
 }
