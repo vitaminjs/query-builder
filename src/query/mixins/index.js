afecf593779319387c9398706b21a58b76daa60b
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2017-07-09 01:08:59 +0100

    Delete unnecessary files

--- src/query/mixins/index.js
+++
@@ -1,8 +0,0 @@
-
-import UseCTE from './use-cte'
-import UseTable from './use-table'
-import UseReturning from './use-returning'
-import UseConditions from './use-conditions'
-
-// export built-in mixins
-export { UseCTE, UseTable, UseReturning, UseConditions }
